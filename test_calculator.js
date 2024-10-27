6d97635c90cbe6c89e71cc204252f77fbe2337bf
emilvn <emilvnielsen@hotmail.com>
2024-10-27 11:36:43 +0100

    better test example

--- test_calculator.js
+++ test_calculator.js
@@ -2,10 +2,20 @@ import expressionCalculator from "./calculator.js";
 import Queue from "./queue.js";
 
 const inputQueue = new Queue();
+inputQueue.enqueue("(");
 inputQueue.enqueue(2);
 inputQueue.enqueue("+");
 inputQueue.enqueue(3);
+inputQueue.enqueue(")");
 inputQueue.enqueue("*");
 inputQueue.enqueue(4);
+inputQueue.enqueue("-");
+inputQueue.enqueue(5);
+inputQueue.enqueue("/");
+inputQueue.enqueue("(");
+inputQueue.enqueue(1);
+inputQueue.enqueue("+");
+inputQueue.enqueue(1);
+inputQueue.enqueue(")");
 
-console.log(expressionCalculator(inputQueue));
+console.log(expressionCalculator(inputQueue)); // should be 17.5
